371caf7388c20e1f90dfcb386458066f59e56672
cleverprogrammer <>
2023-04-01 00:59:59 +0200

    modif next config images

--- next.config.js
+++ next.config.js
@@ -1,10 +1,13 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
   images: {
-    domains: ['lh3.googleusercontent.com', 'gateway.ipfscdn.io'],
-    unoptimized: true,
-  }
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "**",
+      },
+    ],
+  },
 }
 
 module.exports = nextConfig
